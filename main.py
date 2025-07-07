adcd16e949e5002a27455d52dff18d042abc1919
ilhmpbta <178406185+ilhmpbta@users.noreply.github.com>
2025-07-07 20:34:06 +0700

    feat: add CLI add-habit and JSON storage

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from habits import tracker
+
+def print_help():
+    print("Usage:")
+    print("  python main.py add \"habit name\"")
+    print("  python main.py done \"habit name\"")
+    print("  python main.py history")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print_help()
+        sys.exit(1)
+
+    cmd = sys.argv[1]
+
+    if cmd == "add":
+        if len(sys.argv) < 3:
+            print("Please provide a habit name.")
+        else:
+            habit_name = sys.argv[2]
+            tracker.add_habit(habit_name)
+
+    elif cmd == "done":
+        print("Coming soon: mark habit as done!")
+
+    elif cmd == "history":
+        print("Coming soon: view habit history!")
+
+    else:
+        print(f"Unknown command: {cmd}")
+        print_help()
